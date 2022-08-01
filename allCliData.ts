811aeade7b576569cd5f9a51240d4c393fa1d1ef
daning <1540097293@qq.com>
2022-08-01 22:38:15 +0800

    feat: :sparkles: 合并当前的初始化版本的代码

---
+++ allCliData.ts
@@ -0,0 +1,7 @@
+enum cliTypes {
+    "v3ts" = "default-vue3-vite-ts-eslint-babel",
+    "v3nots" = "vue3-nots-vite-eslint-babel",
+    "v3tsp" = "vue3-webpack-ts-eslint-babel",
+    "v2pformat" = "vue2-webpack-eslint-babel",
+    
+}
