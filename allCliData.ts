d44dedaf2feb8469c777688c4910f3dbe95d7ef4
daning <1540097293@qq.com>
2022-08-02 18:15:42 +0800

    feat: :sparkles: 合入新增的koa2框架

--- allCliData.ts
+++ allCliData.ts
@@ -3,5 +3,6 @@ enum cliTypes {
     "v3nots" = "vue3-nots-vite-eslint-babel",
     "v3tsp" = "vue3-webpack-ts-eslint-babel",
     "v2pformat" = "vue2-webpack-eslint-babel",
-    
+    "koa2viewejs"="koa2-ts-view-ejs",
+    "koa2notsviewejs"="koa2-view-ejs"
 }
