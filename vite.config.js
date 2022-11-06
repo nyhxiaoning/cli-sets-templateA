94705c6002889eeecbcb6e58735ca468c0095767
daning <1540097293@qq.com>
2022-11-06 20:28:45 +0800

    vue3的vite配置说明

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ import { visualizer } from 'rollup-plugin-visualizer';
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
+    // 配置
     alias: {
       "@": path.resolve(__dirname, "src"),
       "components": path.resolve(__dirname, "src/components"),
