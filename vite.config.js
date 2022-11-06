301ef04e35b3b6898bcc3f8c473a316f1e6d8695
daning <1540097293@qq.com>
2022-11-06 20:04:05 +0800

    vue3的alias配置

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,19 @@ import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "components": path.resolve(__dirname, "src/components"),
+      "styles": path.resolve(__dirname, "src/styles"),
+      "plugins": path.resolve(__dirname, "src/plugins"),
+      "views": path.resolve(__dirname, "src/views"),
+      "layouts": path.resolve(__dirname, "src/layouts"),
+      "utils": path.resolve(__dirname, "src/utils"),
+      "apis": path.resolve(__dirname, "src/apis"),
+      "dirs": path.resolve(__dirname, "src/directives"),
+    },
+  },
   plugins: [
     vue(),
     // visualizer({  //    build后的视图
